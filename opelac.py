fd0a2b263d6f36f699992e153799df4084cea782
Christian Chimezie <chris.chimezie@gmail.com>
2020-09-12 19:12:14 -0400

    Clean up global variable definitions

--- opelac.py
+++ opelac.py
@@ -1,28 +1,27 @@
-import numpy as np
+from numpy import loadtxt
 from sys import argv
 
-actions = []		# list to log elevator actions
+CALLS_FILE = argv[1]  									# path to input file => {time} {floor} {dest} on each line
+GOTO_TEMP = "TIME {:.2f}\tGOTO FLOOR {}"				# GOTO action template string
+INITIAL_POS = 1											# elevator starts on floor 1
+MOVE_SPEED = 1											# 1 floor/second
+
+dtype = [('time', 'f'), ('floor', 'i'), ('dest', 'i')]  # "f" is for float, "i" is for integer
+calls = loadtxt(fname=CALLS_FILE, dtype=dtype)  		# [(time1, floor1, dest1), (time2, floor2, dest2), ...]
+calls = sorted(calls, key=lambda x: x[0])  				# sort calls by time
+
+actions = []				# list to log elevator actions
+elapsed_time = calls[0][0]	# the time of the very first call
 elevator = {
-	'pos': 1		# elevator starts on floor 1
+	'pos': INITIAL_POS
 }
-move_speed = 1		# 1 floor/second
-elapsed_time = 0.0
-
-GOTO_TEMP = "TIME {}\tGOTO FLOOR {}"	# GOTO action string template
-INITIAL_POS = elevator['pos']
-
-def read_calls():	
-	CALLS_FILE = argv[1]  									# path to input file => {time} {floor} {dest} on each line
-	dtype = [('time', 'f'), ('floor', 'i'), ('dest', 'i')]  # "f" is for float, "i" is for integer
-	calls = np.loadtxt(fname=CALLS_FILE, dtype=dtype)  		# [(time1, floor1, dest1), (time2, floor2, dest2), ...]
-	return sorted(calls, key=lambda x: x[0])  				# sort calls by time
 
 def goto(floor):
 	global elapsed_time
 	current_pos = elevator['pos']
-	time_delta = abs(floor - current_pos) / move_speed  			# time needed to move from current_pos to floor
+	time_delta = abs(floor - current_pos) / MOVE_SPEED  			# time needed to move from current_pos to floor
 	
-	actions.append( GOTO_TEMP.format(elapsed_time, floor) )			# log the action to be performed action
+	actions.append( GOTO_TEMP.format(elapsed_time, floor) )			# log the action to be performed
 	elapsed_time += time_delta										# "wait until elevator arrives at floor"
 	elevator['pos'] = floor											# update elevator position
 
@@ -37,7 +36,7 @@ def dropoff(floor):
 	pickup(floor)													# in this simplified system, picking up is the same as dropping off
 
 # sequentially perform pick ups and drop offs
-for time, pos, dest in read_calls():
+for time, pos, dest in calls:
 	pickup( pos )
 	dropoff( dest )
 
