f5b8b0a18bf3a3450258963b01a408a3279f66c8
Christian Chimezie <chris.chimezie@gmail.com>
2020-09-12 22:58:17 -0400

    Peform major code refactoring

--- opelac.py
+++ opelac.py
@@ -1,4 +1,4 @@
-from numpy import loadtxt
+from numpy import loadtxt, mean, array
 from sys import argv
 
 CALLS_FILE = argv[1]  									# path to input file => {time} {floor} {dest} on each line
@@ -11,34 +11,34 @@ calls = loadtxt(fname=CALLS_FILE, dtype=dtype)  		# [(time1, floor1, dest1), (ti
 calls = sorted(calls, key=lambda x: x[0])  				# sort calls by time
 
 actions = []				# list to log elevator actions
+wait_time = []				# array of pickup time_delta values
+inside_time = []			# array of dropoff time_delta values
 elapsed_time = calls[0][0]	# the time of the very first call
 elevator = {
 	'pos': INITIAL_POS
 }
 
-def goto(floor):
-	global elapsed_time
+def goto(floor, pickup=True):
+	global elapsed_time, wait_time, inside_time
 	current_pos = elevator['pos']
 	time_delta = abs(floor - current_pos) / MOVE_SPEED  			# time needed to move from current_pos to floor
+
+	if pickup:														# log time_delta in appropriate time_delta_list (wait_time/inside_time)
+		wait_time.append( time_delta )
+	else:  															# if not pickup, must be dropoff
+		inside_time.append( time_delta )
 	
 	actions.append( GOTO_TEMP.format(elapsed_time, floor) )			# log the action to be performed
 	elapsed_time += time_delta										# "wait until elevator arrives at floor"
 	elevator['pos'] = floor											# update elevator position
 
-def pickup(floor):
-	global elapsed_time
-	goto(floor)
-	time_delta = 30 if floor == 1 else 5							# time needed to pick up riders
-
-	elapsed_time += time_delta										# "wait until finished loading passengers"
-
-def dropoff(floor):
-	pickup(floor)													# in this simplified system, picking up is the same as dropping off
+	time_delta = 30 if floor == 1 else 5							# time needed to pick up or drop off riders
+	elapsed_time += time_delta
 
 # sequentially perform pick ups and drop offs
 for time, pos, dest in calls:
-	pickup( pos )
-	dropoff( dest )
+	goto( pos )														# goto, then pick up at pos
+	goto( dest, pickup=False )										# goto, then drop off at dest
 
 # write elevator action time series to stdout
 print( "[START FLOOR {}]".format(INITIAL_POS) )
